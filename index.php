c9f55c9591d0b0d0baf05c5e10abc30efdbb5717
alumno <alumno>
2017-03-21 21:21:55 -0300

    Indices de Array

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
     //echo "Hola Mundo";
     //var_dump
-
+    /*
+    Aplicación Nº 6 (Carga aleatoria)
+    Definir un Array de 5 elementos enteros y asignar a cada uno de ellos un número (utilizar la
+    función rand ). Mediante una estructura condicional, determinar si el promedio de los números
+    son mayores, menores o iguales que 6. Mostrar un mensaje por pantalla informando el
+    resultado.
+    */
+    //Constructor Array
+    /*
     $numeros = array(
         rand(1,100),
         rand(1,100),
@@ -9,7 +17,17 @@
         rand(1,100),
         rand(1,100)
     );
+    */
 
-   
+    //Indices de Array
+    $numeros = array();
+
+    for($i=0; $i<5; $i++)
+    {
+        $numeros[$i] = rand(1, 100);
+    }
+
     var_dump($numeros);
+
+
 ?>
